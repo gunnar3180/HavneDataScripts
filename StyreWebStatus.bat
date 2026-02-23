7d69aa35d2392fbc6010e23fb09025556093bb38
Gunnar Bjelland <gunnar3180@gmail.com>
2026-02-23 09:45:51 +0100

    Tilpasning til Solviken-PC og automatisk kjøring om natta

--- StyreWebStatus.bat
+++ StyreWebStatus.bat
@@ -1,14 +1,15 @@
 @echo off
+cd /d "%~dp0"
 
 echo Henter data fra StyreWeb, vent...
 "C:\Program Files (x86)\LINQPad5\lprun.exe" WebAutomation.linq
 
 echo(
 echo Status båtplasser:
-set download_folder=C:\Users\Solviken\Downloads
+set download_folder=C:\Users\Solvi\Downloads
 set status_file=%download_folder%\StyreWebStatus.txt
 "C:\Program Files (x86)\LINQPad5\lprun.exe" Havneberegninger.linq -file "%status_file%"
 
 echo(
 echo Styreweb båtplass-status lagret i %status_file%
-pause
+timeout /t 10
